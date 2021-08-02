998ae8d7c1bba27476552712c5b44ea9ef6da0fb
Tomas Härdin <tjoppen@acc.umu.se>
2021-08-02 20:02:37 +0200

    Det funkar
    road_pumping_station_v2.ini måste dubbelkollas

--- workers_and_resources.py
+++ workers_and_resources.py
@@ -2,33 +2,135 @@
 import os
 import struct
 
-'''
-.bbox format
-
-u32 numshapes;
-{
-  //540 B per shape
-  char shapename[512]; //at least 29 (pasted__polySurfaceShape1430), lots of strcpy()-ish garbage and uninited memory
-  u32 index;           //appears to increase by 1 per shape, starting from 0
-  float xmin;
-  float ymin;
-  float zmin;
-  float xmax;
-  float ymax;
-  float zmax;
-}
-'''
-
 d = 'media_soviet/buildings_types'
 goods = set()
 #COST_WORK_BUILDING_NODE = set()
-COST_RESOURCE_AUTO = set()
-COST_RESOURCE_AUTO2 = {}
-COST_RESOURCE_AUTO3 = {}
+#COST_RESOURCE_AUTO = set()
+#COST_RESOURCE_AUTO2 = {}
+#COST_RESOURCE_AUTO3 = {}
 n = 0
+
+# other crap to ignore
+ignore = [
+  '$CONNECTION',
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  'end',
+  '-',
+  '$NAME',
+  '$HARBOR',
+  '$HEATING_DISABLE',
+  '$ATTRACTIVE',
+  '$STORAGE',
+  '$TYPE',
+  '$SEASONAL',
+  '$ELETRIC_WITHOUT',
+  '$SUBTYPE_TELEVISION',
+  '$PROFESORS_NEEDED',
+  '$TEXT_CAPTION',
+  '$VEHICLE_STATION',
+  '$RESOURCE_VISUALIZATION',
+  'position',
+  'rotation',
+  'scale',
+  'numstepx',
+  'numstept',
+  '$RESOURCE_INCREASE_POINT',
+  '$VEHICLE_UNLOADING_FACTOR',
+  '$ELETRIC_CONSUMPTION_LOADING_FIXED',
+  '$ELETRIC_CONSUMPTION_UNLOADING_FIXED',
+  '$STATION_NOT_BLOCK',
+  '$QUALITY_OF_LIVING',
+  '$WORKING_VEHICLES_NEEDED',
+  '$VEHICLE_PARKING',
+  '$PARTICLE',
+  '$VEHICLE_CANNOTSELECT_INSIDE',
+  '$LONG_TRAINS',
+  'positon',
+  '$HELIPORT_STATION',
+  '$WORKER_RENDERING_AREA',
+  '$SUBTYPE_TROLLEYBUS',
+  '$ROADVEHICLE_ELETRIC',
+  '$VEHICLE_PARKING_PERSONAL',
+  '$SUBTYPE_AIRPLANE',
+  '$HEATING_ENABLE',
+  '$SUBTYPE_AIR',
+  '$SUBTYPE_AIRCUSTOM',
+  '$CIVIL_BUILDING',
+  #'$COST_WORK_BUILDING_ALL',
+  '$ROADVEHICLE_NOTFLIP',
+  '$ELETRIC_CONSUMPTION_LIVING_WORKER_FACTOR',
+  '$ELETRIC_CONSUMPTION_LIGHTING_WORKER_FACTOR',
+  '$OFFSET',
+  '$RESOURCE',
+  '//',
+  '$WORKING_SFX',
+  '$PRODUCTION_CONNECT_TO_SUN',
+  '$HELIPORT_AREA',
+  '$ELETRIC_CONSUMPTION_HEATING_WORKER_FACTOR',
+  '$VEHICLE_LOADING_FACTOR',
+  '$POLLUTION_SMALL',
+  '$SUBTYPE_CABLEWAY',
+  '$ENGINE_SPEED',
+  '$POLLUTION_MEDIUM',
+  '$SUBTYPE_CABLEWAY',
+  '$CABLEWAY_LIGHT',
+  'buildings',
+  '$VEHICLE_PASSANGER_ONLY',
+  '$SUBTYPE_SHIP',
+  '$SHIP_STATION',
+  '$SUBTYPE_RADIO',
+  '$UNDERGROUND_MESH',
+  '$MONUMENT_ENABLE_TRESPASSING',
+  '$SUBTYPE_TECHNICAL',
+  '$ANIMATION_MESH',
+  'editor',
+  '$SUBTYPE_HOSTEL',
+  '$HEATING_WITHOUT_WORKING_FACTOR',
+  '$CABLEWAY_HEAVY',
+  '$SUBTYPE_SOVIET',
+  '$SUBTYPE_ROAD',
+  '$PRODUCTION_CONNECT_TO_WIND',
+  '$ANIMATION_SPEED_FPS',
+  '$SUBTYPE_MEDICAL',
+  '$FIRESTATION',
+  '$SUBTYPE_RESTAURANT',
+  'televizia',
+  '$SUBTYPE_RAIL',
+  '$SUBTYPE_SPACE_FOR_VEHICLES',
+]
 
 # hard mode, 1960
 autos = ['tech_steel', 'wall_steel', 'electro_steel', 'steel', 'techelectro_steel', 'ground', 'wall_panels', 'wall_bricks', 'wall_wood', 'roof_woodbrick', 'roof_asphalt', 'wall_concrete', 'wall_brick', 'roof_woodsteel', 'roof_steel', 'ground_asphalt']
+
+# for $COST_RESOURCE
+fields = ['workers', 'concrete', 'gravel', 'asphalt', 'bricks', 'boards', 'steel', 'mcomponents', 'ecomponents', 'prefabpanels']
+
+auto_dict = {
+  # Auto: (Ground, Walls, Volume, Workdays, Concrete, Gravel, Asphalt, Bricks, Boards, Steel, Mech C, Elec C, Prefab)
+  'ground_asphalt':   (1,0,0.08,    150,13,10,8,0,0,0,0,0,0),
+  'electro_steel':    (0,0.25,0.8,  170,0,0,0,0,0,6,0,1.25,0),
+  'tech_steel':       (0,0.25,0.8,  170,0,0,0,0,0,6,1.25,0,0),
+
+  'ground': (1,0,0.08,150,13,10,0,0,0,0,0,0,0),
+  'wall_concrete': (0,1,0.3,100,22,0,0,0,0,5,0,0,0),
+  'wall_panels': (0,1,0.3,65,0,0,0,0,0,1,0,0,10),
+  'wall_brick': (0,1,0.3,140,0,0,0,12,4,1.5,0,0,0),
+  'wall_steel': (0,1,0.3,90,0,0,0,0,0,8,0,0,0),
+  'wall_wood': (0,1,0.3,90,0,0,0,0,10,0,0,0,0),
+  'techelectro_steel': (0,0.25,0.8,190,0,0,0,0,0,5,0.85,0.55,0),
+  'roof_woodbrick': (1,0,0.05,87,0,0,0,2,10,0,0,0,0),
+  'roof_steel': (1,0,0.05,95,0,0,0,0,0,7,0,0,0),
+  'roof_woodsteel': (1,0,0.05,85,0,0,0,0,5,3,0,0,0),
+}
 
 # These are ordered in complexity. Each row has at most row# number of non-zeroes
 # airplaneparking_30.ini -> ground_asphalt
@@ -47,25 +149,6 @@ autos = ['tech_steel', 'wall_steel', 'electro_steel', 'steel', 'techelectro_stee
 # hotel_horsky.ini -> wall_bricks
 # rail_gas_station.ini -> wall_steel
 # co_beach.ini -> wall_wood
-
-'''
-airplaneparking_30.ini
-eletric_switch_low.ini
-road_pumping_station_v2.ini
-powerplant_wind2.ini
-heating_endstation_small.ini
-pedestrian_tunel_entry.ini
-trolleybus_depo.ini
-raildepobig.ini
-playground_tenis2.ini
-fountain3.ini
-co_art_gallery.ini
-warehouse_covered_combined.ini
-production_airplane.ini
-hotel_horsky.ini
-rail_gas_station.ini
-co_beach.ini
-'''
 
 RE = [
 'airplaneparking_30.ini',
@@ -86,23 +169,30 @@ RE = [
 'co_beach.ini',
 ]
 
-skipem = ['ground_asphalt', 'electro_steel', 'tech_steel', 'techelectro_steel', 'wall_brick', 'ground',
-'roof_steel','roof_woodsteel','steel','wall_concrete','roof_asphalt','roof_woodbrick','wall_panels',
-#'wall_bricks','wall_steel','wall_wood',
-]
+#outstats = [()]*16
 
-outstats = [()]*16
+costs_dict = {}
 
 for e in os.listdir(d):
+#for e in ['coal_mine.ini']:
+#for e in RE:
+  # fields have zero cost
+  if e[:6] == 'field_':
+    continue
+
   if '.ini' in e:
     shapes = {}
+    costs = [0]*10
+
+    # parse .bbox into shapes
     with open(d+'/'+e.replace('.ini','.bbox'), 'rb') as f:
       numshapes = struct.unpack('I', f.read(4))[0]
-      COST_RESOURCE_AUTO2[e] = set()
-      auto_mask = '0'*len(autos)
+      #COST_RESOURCE_AUTO2[e] = set()
+      #auto_mask = '0'*len(autos)
 
       #print(e)
       #print(numshapes)
+      '''
       atot = 0
       wtot = 0
       vtot = 0
@@ -113,10 +203,16 @@ for e in os.listdir(d):
       xmaxmax = None
       ymaxmax = None
       zmaxmax = None
+      '''
+      
       for i in range(numshapes):
         name, index, xmin, ymin, zmin, xmax, ymax, zmax = struct.unpack('512sIffffff', f.read(540))
         name = name[0:name.find(b'\0')].decode('ascii')
+        xs = xmax - xmin
+        ys = ymax - ymin
+        zs = zmax - zmin
 
+        '''
         if xminmin is None:
           xminmin = xmin
           yminmin = ymin
@@ -132,10 +228,6 @@ for e in os.listdir(d):
         ymaxmax = max(ymaxmax, ymax)
         zmaxmax = max(zmaxmax, zmax)
 
-        xs = xmax - xmin
-        ys = ymax - ymin
-        zs = zmax - zmin
-
         v = xs*ys*zs # volume
         a = xs*zs # area
         c = 2*xs + 2*zs # circumference
@@ -145,18 +237,10 @@ for e in os.listdir(d):
         atot += a
         ctot += c
         wtot += w
+        '''
 
-        # stuff som troligtvis är relevant:
-        # golvyta på alla bboxes (v)
-        # omkrets på alla bboxes (c)
-        # väggyta på alla bboxes (w)
-        # golvyta som omringar alla bboxes (betonggrund)
-        # volym av alla bboxes (v)
-        # volym som omringar alla bboxes (osäker om användbart)
-
-
-        if 'cement_plant' in e and 'ground' in name:
-          print("   %30s %f" % (name, v))
+        #if 'cement_plant' in e and 'ground' in name:
+        #  print("   %30s %f" % (name, v))
         shapes[name] = (xs, ys, zs)
 
         # ys == 0 happens for flat planes (y = vertical)
@@ -164,10 +248,10 @@ for e in os.listdir(d):
           print('bork')
           exit(1)
       
+      '''
       try:
         #print(RE)
         i = RE.index(e)
-        de här summorna måste göras baserat på vad varje auto specar för bboxar
         outstats[i] = (
           atot,
           ctot,
@@ -182,6 +266,7 @@ for e in os.listdir(d):
       except Exception:
         pass
         #stats[
+      '''
 
     with open(d+'/'+e, 'r') as f:
       n += 1
@@ -192,9 +277,55 @@ for e in os.listdir(d):
       CONSUMPTION_PER_SECOND = []
       
       # building phase stuff
-      COST_WORK_BUILDING_NODE = [] # taken from bboxes
-      COST_WORK_VEHICLE_STATION_ACCORDING_NODE = [] # taken from bboxes
-      COST_WORK_VEHICLE_STATION = [] # defined explicitly in file
+      #COST_WORK_BUILDING_NODE = [] # taken from bboxes
+      #COST_WORK_VEHICLE_STATION_ACCORDING_NODE = [] # taken from bboxes
+      #COST_WORK_VEHICLE_STATION = [] # defined explicitly in file
+
+      autos = {}
+      nodes = set()
+
+      '''
+Airplane parking 30m	242	21	16	12					
+Medium voltage switch	43	1,7	1,3	1			0,85		0,18
+Gas station	125	6,3	4,9	3,9			4,4		
+Big wind power plant	689	3,2	2,5	2			17	2,9	1,9
+      '''
+
+      def handle_autos():
+        #print('handle_autos: ' + str(autos) + ' ' + str(nodes))
+        for auto, weight in autos.items():
+          mul = weight*0.5
+          v = 0
+          g = 0
+          w = 0
+
+          for n in nodes:
+            if n in shapes:
+              xs, ys, zs = shapes[n]
+              v += xs*ys*zs
+              g += xs*zs
+              w += 2.0*(xs+zs)*ys
+            else:
+              #print(n + ' not found in shapes')
+              pass
+
+          g /= 300.0
+          w /= 300.0
+          v /= 3000.0
+
+          # there are typos
+          if auto in auto_dict:
+            a = auto_dict[auto]
+            # Ground, Walls, Volume
+            factor = g*a[0] + w*a[1] + v*a[2]
+            #print(factor)
+
+            for i in range(len(costs)):
+              costs[i] += mul*factor*a[i+3]
+
+          #print(auto, weight)
+          pass
+        pass
 
       for line in f.readlines():
         p = line.split()
@@ -202,104 +333,6 @@ for e in os.listdir(d):
         # ignore empty lines
         if len(p) == 0:
           continue
-
-        # other crap to ignore
-        ignore = [
-          '$CONNECTION',
-          '0',
-          '1',
-          '2',
-          '3',
-          '4',
-          '5',
-          '6',
-          '7',
-          '8',
-          '9',
-          'end',
-          '-',
-          '$NAME',
-          '$HARBOR',
-          '$HEATING_DISABLE',
-          '$ATTRACTIVE',
-          '$STORAGE',
-          '$TYPE',
-          '$SEASONAL',
-          '$ELETRIC_WITHOUT',
-          '$SUBTYPE_TELEVISION',
-          '$PROFESORS_NEEDED',
-          '$TEXT_CAPTION',
-          '$VEHICLE_STATION',
-          '$RESOURCE_VISUALIZATION',
-          'position',
-          'rotation',
-          'scale',
-          'numstepx',
-          'numstept',
-          '$RESOURCE_INCREASE_POINT',
-          '$VEHICLE_UNLOADING_FACTOR',
-          '$ELETRIC_CONSUMPTION_LOADING_FIXED',
-          '$ELETRIC_CONSUMPTION_UNLOADING_FIXED',
-          '$STATION_NOT_BLOCK',
-          '$QUALITY_OF_LIVING',
-          '$WORKING_VEHICLES_NEEDED',
-          '$VEHICLE_PARKING',
-          '$PARTICLE',
-          '$VEHICLE_CANNOTSELECT_INSIDE',
-          '$LONG_TRAINS',
-          'positon',
-          '$HELIPORT_STATION',
-          '$WORKER_RENDERING_AREA',
-          '$SUBTYPE_TROLLEYBUS',
-          '$ROADVEHICLE_ELETRIC',
-          '$VEHICLE_PARKING_PERSONAL',
-          '$SUBTYPE_AIRPLANE',
-          '$HEATING_ENABLE',
-          '$SUBTYPE_AIR',
-          '$SUBTYPE_AIRCUSTOM',
-          '$CIVIL_BUILDING',
-          '$COST_WORK_BUILDING_ALL',
-          '$ROADVEHICLE_NOTFLIP',
-          '$ELETRIC_CONSUMPTION_LIVING_WORKER_FACTOR',
-          '$ELETRIC_CONSUMPTION_LIGHTING_WORKER_FACTOR',
-          '$OFFSET',
-          '$RESOURCE',
-          '//',
-          '$WORKING_SFX',
-          '$PRODUCTION_CONNECT_TO_SUN',
-          '$HELIPORT_AREA',
-          '$ELETRIC_CONSUMPTION_HEATING_WORKER_FACTOR',
-          '$VEHICLE_LOADING_FACTOR',
-          '$POLLUTION_SMALL',
-          '$SUBTYPE_CABLEWAY',
-          '$ENGINE_SPEED',
-          '$POLLUTION_MEDIUM',
-          '$SUBTYPE_CABLEWAY',
-          '$CABLEWAY_LIGHT',
-          'buildings',
-          '$VEHICLE_PASSANGER_ONLY',
-          '$SUBTYPE_SHIP',
-          '$SHIP_STATION',
-          '$SUBTYPE_RADIO',
-          '$UNDERGROUND_MESH',
-          '$MONUMENT_ENABLE_TRESPASSING',
-          '$SUBTYPE_TECHNICAL',
-          '$ANIMATION_MESH',
-          'editor',
-          '$SUBTYPE_HOSTEL',
-          '$HEATING_WITHOUT_WORKING_FACTOR',
-          '$CABLEWAY_HEAVY',
-          '$SUBTYPE_SOVIET',
-          '$SUBTYPE_ROAD',
-          '$PRODUCTION_CONNECT_TO_WIND',
-          '$ANIMATION_SPEED_FPS',
-          '$SUBTYPE_MEDICAL',
-          '$FIRESTATION',
-          '$SUBTYPE_RESTAURANT',
-          'televizia',
-          '$SUBTYPE_RAIL',
-          '$SUBTYPE_SPACE_FOR_VEHICLES',
-        ]
         
         if any([i for i in ignore if i in p[0]]):
           continue
@@ -329,25 +362,33 @@ for e in os.listdir(d):
         elif p[0] == '$COST_WORK':
           # new building phase
           # $COST_WORK SOVIET_CONSTRUCTION_GROUNDWORKS 0.0
-          # $COST_WORK SOVIET_CONSTRUCTION_SKELETON_CASTING		1.0
-          COST_WORK_BUILDING_NODE = []
-          COST_WORK_VEHICLE_STATION_ACCORDING_NODE = []
-          COST_WORK_VEHICLE_STATION = []
+          # $COST_WORK SOVIET_CONSTRUCTION_SKELETON_CASTING,0,1.0
+          #COST_WORK_BUILDING_NODE = []
+          #COST_WORK_VEHICLE_STATION_ACCORDING_NODE = []
+          #COST_WORK_VEHICLE_STATION = []
+          
+          handle_autos()
+          autos = {}
+          nodes = set()
         elif p[0] == '$COST_RESOURCE_AUTO':
-          # $COST_RESOURCE_AUTO ground_asphalt 	1.0
+          # $COST_RESOURCE_AUTO ground_asphalt ,1.0
           # $COST_RESOURCE_AUTO wall_steel 1.0
           # $COST_RESOURCE_AUTO tech_steel 0.8
-          if p[1] not in skipem and 'residental' not in e and 'panel' not in e:
-            ps = p[1].split('_')
-            pos = autos.index(p[1])
-            auto_mask = auto_mask[:pos] + '1' + auto_mask[pos+1:]
-            COST_RESOURCE_AUTO.add(p[1])
-            COST_RESOURCE_AUTO2[e].add(p[1])
+          #ps = p[1].split('_')
+          #pos = autos.index(p[1])
+          #auto_mask = auto_mask[:pos] + '1' + auto_mask[pos+1:]
+          #COST_RESOURCE_AUTO.add(p[1])
+          #COST_RESOURCE_AUTO2[e].add(p[1])
+          autos[p[1]] = float(p[2])
+          pass
         elif p[0] == '$COST_RESOURCE':
+          # $COST_RESOURCE workers 3000
+          costs[fields.index(p[1])] += float(p[2])
           #print(p)
           pass
         elif p[0] == '$COST_WORK_BUILDING_NODE':
           # $COST_WORK_BUILDING_NODE concreteShape7
+          nodes.add(p[1])
           pass
         elif p[0] == '$COST_WORK_VEHICLE_STATION_ACCORDING_NODE':
           # $COST_WORK_VEHICLE_STATION_ACCORDING_NODE polySurfaceShape278
@@ -359,16 +400,35 @@ for e in os.listdir(d):
           # $COST_WORK_BUILDING_KEYWORD $concrete
           # $COST_WORK_BUILDING_KEYWORD $steel
           # $COST_WORK_BUILDING_KEYWORD $tech
-          pass
+          # this just matches on the start of the node names
+          # unless the keyword is $all
+          keyword = p[1][1:]
+          if keyword == 'all':
+            for k in shapes.keys():
+              nodes.add(k)
+              #print('all')
+          else:
+            n = len(keyword)
+            for k in shapes.keys():
+              if k[:n] == keyword:
+                nodes.add(k)
+        elif p[0] == '$COST_WORK_BUILDING_ALL':
+          # same as $all
+          for k in shapes.keys():
+            nodes.add(k)
         else:
           # print stuff we missed
           print(e, p)
 
+
+      handle_autos()
+      costs_dict[e] = costs
+
       #if WORKERS_NEEDED and PRODUCTION:
       #  print(e, WORKERS_NEEDED, str(CONSUMPTION), '->', PRODUCTION)
-      COST_RESOURCE_AUTO3[str(len(COST_RESOURCE_AUTO2[e]))+','.join(sorted(list(COST_RESOURCE_AUTO2[e])))] = e
+      #COST_RESOURCE_AUTO3[str(len(COST_RESOURCE_AUTO2[e]))+','.join(sorted(list(COST_RESOURCE_AUTO2[e])))] = e
       #COST_RESOURCE_AUTO3[str(len(COST_RESOURCE_AUTO2[e]))+auto_mask] = e
-      COST_RESOURCE_AUTO2[e] = list(COST_RESOURCE_AUTO2[e])
+      #COST_RESOURCE_AUTO2[e] = list(COST_RESOURCE_AUTO2[e])
       #print(auto_mask)
 
 #for g in goods:
@@ -383,7 +443,11 @@ import json
 from collections import OrderedDict
 #print(json.dumps(OrderedDict(sorted(COST_RESOURCE_AUTO3.items())), indent=4))
 #print(len(COST_RESOURCE_AUTO3))
-for k,v in sorted(COST_RESOURCE_AUTO3.items()):
-  print("# %s -> %s" % (v, k[1:]))
-print(len(skipem))
-print(json.dumps(outstats[RE.index('playground_tenis2.ini')], indent=4))
+#for k,v in sorted(COST_RESOURCE_AUTO3.items()):
+#  print("# %s -> %s" % (v, k[1:]))
+#print(json.dumps(outstats[RE.index('playground_tenis2.ini')], indent=4))
+#print(json.dumps(costs_dict, indent=4))
+for k, v in costs_dict.items():
+  print('%37s %s' % (k, ' '.join(['%7.1f' % (round(vv*10)*0.1) for vv in v])))
+print(len(costs_dict))
+
