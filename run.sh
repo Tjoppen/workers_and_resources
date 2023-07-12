381bc9d4c997f84f38ca9158ce35fa1b5e100f41
Tomas Härdin <git@haerdin.se>
2023-07-12 22:17:03 +0200

    Add AGPL license

--- run.sh
+++ run.sh
@@ -1,3 +1,19 @@
+#    workers_and_resources
+#    Copyright (C) 2023  Tomas Härdin
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
 set -e
 ./generate_lp.py > /tmp/program.lp && time lp_solve -S2 -v -presolve -presolver -g 1e3 < /tmp/program.lp | tee solution.txt
 grep "Value of objective function" solution.txt
